8eb39f626bddd86987a7b755f5850866dbcdee9f
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2024-05-28 11:05:03 -0500

    vpc testing

--- module-alb.tf
+++ module-alb.tf
@@ -1,13 +1,13 @@
-# https://registry.terraform.io/modules/terraform-aws-modules/alb/aws/latest
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "9.9.0"
+# # https://registry.terraform.io/modules/terraform-aws-modules/alb/aws/latest
+# module "alb" {
+#   source  = "terraform-aws-modules/alb/aws"
+#   version = "9.9.0"
 
-  name = "my-alb"
+#   name = "my-alb"
 
-  vpc_id = module.vpc.default_vpc_id
+#   vpc_id = module.vpc.default_vpc_id
 
-  subnets = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
+#   subnets = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
 
 
-}
+# }
