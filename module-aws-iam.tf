1e9eec91f7a5005672f491b449bb2c8925e85cc7
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2024-06-23 13:12:00 -0500

    iam role update

--- module-aws-iam.tf
+++ module-aws-iam.tf
@@ -1,5 +1,29 @@
-# #https://registry.terraform.io/modules/terraform-aws-modules/iam/aws/latest
-# module "iam" {
-#   source  = "terraform-aws-modules/iam/aws"
-#   version = "5.39.1"
-# }
+module "iam" {
+  source  = "terraform-aws-modules/iam/aws"
+  version = "5.39.1"
+
+}
+
+module "iam_policy" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
+
+  name        = "Role_for_Terraform_s3"
+  path        = "/"
+  description = "My Terraform S3 policy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:*",
+                "s3-object-lambda:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
