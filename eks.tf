7b1dcf701ca2b2d89bede274b0515ade260932b2
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2024-09-21 10:24:24 -0500

    eks-setup

--- eks.tf
+++ eks.tf
@@ -13,12 +13,12 @@
 #   create_iam_role = true
 #   iam_role_name   = "vicky_eks"
 
-#   eks_managed_node_group_defaults = {
+#   eks_managed_node_groups = {
 #     default = {
 
 #       min_size     = 1
 #       max_size     = 3
-#       desired_size = 2
+#       desired_size = 1
 
 #       instance_types = ["t3.medium"]
 #       capacity_type  = "ON_DEMAND"
